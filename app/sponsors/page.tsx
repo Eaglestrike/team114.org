a67164b27d0a343d1cd873136b4f34dde847b8dd
virshah <virn.shah@gmail.com>
2024-06-02 17:29:59 -0700

    Links sponsorship prospectus on website

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -9,15 +9,18 @@ export default function SponsorsPage() {
 	return (
 		<div>
 			<p className={'text-5xl m-4 pb-6'}>Sponsors</p>
-			<div className={'p-4 bg-slate-900 m-4 rounded-2xl'}>
-				<p className={'text-center font-semibold text-white'}>
+			<div className={'p-4 bg-slate-900 m-4 rounded-md max-w-6xl mx-auto'}>
+				<p className={'text-center text-3xl text-white'}>
 					Team 114 would not be possible without the generous donation of our sponsors.
+				</p>
+				<p className={'text-center text-white mt-2'}>
+					Interested? Read more here: <a href={'https://docs.google.com/document/d/1bZPDlrVWRtjtCxCHdrQo8Cva77OVnZ_W/edit'} className={'underline hover:text-blue-500'}>Sponsorship Prospectus</a>
 				</p>
 			</div>
 			<div id={'partners'}>
 				<SectionTitle title={'Partners ($5000+)'} color={'rose-600'} />
 				<div className={'grid grid-cols-2 gap-x-8 p-4'}>
-					{partners.map((partner) => (
+				{partners.map((partner) => (
 						<a
 							key={partner.name}
 							href={partner.website}
