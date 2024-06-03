9344a30732ca6005a416c314f80cd780a2249ee0
virshah <virn.shah@gmail.com>
2024-06-02 17:42:35 -0700

    Adds google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+		<GoogleTagManager gtmId="G-DTSRV8YCHM" />
+		<body className={inter.className}>
 				<nav
 					className={
 						'flex flex-row justify-end gap-x-5 text-white p-4 bg-slate-900 bg-opacity-95 uppercase font-semibold'
