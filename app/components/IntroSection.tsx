c3996f62c3b1ab438e835e5a855ba3ddfab495a8
alaninnovates <alanlichen1@gmail.com>
2025-03-07 11:50:57 -0800

    Footer updates and background color is less intense

--- app/components/IntroSection.tsx
+++ app/components/IntroSection.tsx
@@ -3,7 +3,7 @@ import { foughtKnight } from '../fonts';
 
 export default function IntroSection() {
 	return (
-		<main className={'bg-blue-600 py-16 md:py-24 pb-10 text-white'}>
+		<main className={'bg-blue-500/70 py-16 md:py-24 pb-10 text-white'}>
 			<div className={'flex flex-col items-center w-full'}>
 				<Image src={'/eagle.png'} alt={'Eagle'} width={300} height={300} />
 				<p className={`uppercase text-center mt-10 mb-6 text-8xl ${foughtKnight.className}`}>EagleStrike</p>
