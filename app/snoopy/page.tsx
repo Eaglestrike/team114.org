1509394a1d221602397bc80e964f3d642e73718e
Edward Liu <edward.liu@team114.org>
2025-03-20 16:24:41 -0700

    added pardon again :(

--- app/snoopy/page.tsx
+++ app/snoopy/page.tsx
@@ -4,10 +4,8 @@ import Robo3d from '@/app/components/Robo3d'
 export default function SnoopyPage() {
 	return (
 		<div className="w-screen bg-slate-800 flex flex-row">
-			{/* Left Fixed Section */}
 				<Robo3d/>
 
-			{/* Right Scrolling Content (Part of Normal Page Flow) */}
 			<div className="w-1/2 ml-1/2 flex flex-col">
 				<div className="py-24 px-10">amogus</div>
 				<div className="py-24 px-10">amogus</div>
